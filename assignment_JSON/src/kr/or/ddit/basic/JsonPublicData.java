053bfae100c07925a774c9b9769693adfbcfd6f5
DESKTOP-7JAGA93\pc24 <68523994+shfkd123@users.noreply.github.com>
2021-04-03 12:34:14 +0900

    HighJava
    Json

--- assignment_JSON/src/kr/or/ddit/basic/JsonPublicData.java
+++ assignment_JSON/src/kr/or/ddit/basic/JsonPublicData.java
@@ -13,15 +13,14 @@ import org.json.simple.parser.ParseException;
 
 
 public class JsonPublicData {
+
 	private String svcKey = "Grid_20180109000000000575_1"; // 음식점 업종별 매출액 분포
 	private String apiKey = "d0027a85b2edbf32c7f69e6292fe7a42ec8342df2d090c10b7abb0f254b75a20"; // 개인별 발급.
 	private String startIdx = "1"; // 요청시작위치
 	private String endIdx = "5";
 	// 요청종료위치
-	private String restaurant_Type = "치킨전문점"; //알고싶은 치킨전문점의 매출액
+	private int year = 2018; //년도
 	 
-	
-
 	/**
 	 * JSON
 	 * 
@@ -30,11 +29,12 @@ public class JsonPublicData {
 	 * @throws ParseException
 	 */
 	private JSONObject getJSONObject() throws IOException, ParseException {
-		URL url = new URL("http://211.237.50.150:7080/openapi" + apiKey + "/json/" + svcKey + "/" + startIdx + "/"
-				+ endIdx + "?RESTAURANT_TYPE=" + restaurant_Type);
+		URL url = new URL("http://211.237.50.150:7080/openapi/" + apiKey + "/json/" + svcKey + "/" + startIdx + "/"
+				+ endIdx + "?YEAR=" + 2015);
 		URLConnection urlConnection = url.openConnection();
 
 		System.out.println(url.toString());
+		
 		JSONParser parser = new JSONParser();
 
 		Object obj = parser.parse(new InputStreamReader(urlConnection.getInputStream()));
@@ -55,9 +55,9 @@ public class JsonPublicData {
 
 		JSONObject rootObj = (JSONObject) jsonfile.get(svcKey); // svcKey : value값
 
-		long totalCnt = (long) rootObj.get("totalCnt"); // 전체 레시피 재료 수
+		long totalCnt = (long) rootObj.get("totalCnt"); // 전체 자료수
 
-		endIdx = totalCnt + ""; // 레시피 재료 마지막 순번 설정
+		endIdx = totalCnt + ""; 
 		// -----------------------------------------------------------------------
 		// 구해온 전체 재료수를 이용하여 다시 요청함.
 
@@ -68,22 +68,9 @@ public class JsonPublicData {
 		JSONObject result = (JSONObject) rootObj.get("result");
 		String code = (String) result.get("code");
 
-		if (code.equals("INFO-000")) { // 정상 상태이면...
+		if (code.equals("INFO-000")) { // 정상 상태코드 INFO-000
 
 			JSONArray list = (JSONArray) rootObj.get("row"); // row안에는 json배열이 들어있다.
-
-			// for(Object tempObj : list) {
-			//
-			// JSONObject tempJson = (JSONObject) tempObj;
-			//
-			// System.out.println("순번 : " + tempJson.get("ROW_NUM"));
-			// System.out.println("레시피ID : " + tempJson.get("RECIPE_ID"));
-			// System.out.println("재료명 : " + tempJson.get("IRDNT_NM"));
-			// System.out.println("용량 : " + tempJson.get("IRDNT_CPCTY"));
-			// System.out.println("재료구분 : " + tempJson.get("IRDNT_TY_NM"));
-			//
-			// System.out.println("-------------------------");
-			// }
 
 			@SuppressWarnings("unchecked")
 			Iterator<JSONObject> it = list.iterator();
@@ -92,11 +79,14 @@ public class JsonPublicData {
 
 				JSONObject tempJson = it.next(); // row안에서 꺼내온 json배열
 
-				System.out.println("년도 : " + tempJson.get("YEAR"));
-				System.out.println("서비스 유형 :" + tempJson.get("SVC_TY"));
-				System.out.println("업체수 : " + tempJson.get("ENTRPS_CO"));
-				System.out.println("매출랙 대비 영업이익율 : " + tempJson.get("BSN_PROFITRT"));
-				System.out.println("매출대비 식재료 및 인건비 : " + tempJson.get("PLTCHRGE_LBCST_RT"));
+				System.out.println("분류 : " + tempJson.get("CL"));
+				System.out.println("사례수 :" + tempJson.get("CASE_CO"));
+				System.out.println("5천만원 미만 : " + tempJson.get("FIVE_TH_WON_BELO"));
+				System.out.println("5천만원 ~1억미만  : " + tempJson.get("ONE_HM_BELO"));
+				System.out.println("1억 ~ 5억 미만  : " + tempJson.get("FIVE_HM_BELO"));
+				System.out.println("5억 이상  : " + tempJson.get("FIVE_HM_ABNRML"));
+				System.out.println("평균(만원)  : " + tempJson.get("AVRG_AMOUNT"));
+				System.out.println("단위  : " + tempJson.get("UNIT"));
 
 				System.out.println("-------------------------");
 			}
